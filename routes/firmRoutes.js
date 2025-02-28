9faa47c27f7c7d1ad1c47a2d63a97433bb99a087
PavanK152 <neikora0245@gmail.com>
2025-02-28 21:33:20 +0530

    first commit

---
+++ routes/firmRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const firmController = require('../controllers/firmcontroller');
+const verifyToken = require('../middlewares/verifyToken');
+
+const router = express.Router()
+
+router.post('/add-firm',verifyToken, firmController.addFirm);
+
+router.get('/upload/:imageName', (req,res)=>{
+    const imageName = req.params.imageName;
+    req.headersSent('Content-Type', 'image/jpeg');
+    req.sendFile(path.join(__dirname, '..', 'uploads', imageName));
+
+});
+
+router.delete('/firmId', firmController.deleteFirmById);
+module.exports = router;
