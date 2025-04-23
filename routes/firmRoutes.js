b81fd8b74922194266507508cf6d1f42e53650e7
PavanK152 <neikora0245@gmail.com>
2025-04-23 06:51:00 +0530

    changes in models

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const firmController = require("../controllers/firmController");
+
 const firmController = require("../controllers/firmController");
 
 const verifyToken = require("../middlewares/verifyToken");
@@ -13,8 +13,6 @@ router.get("/upload/:imageName", (req, res) => {
 
   req.headersSent("Content-Type", "image/jpeg");
   req.sendFile(path.join(__dirname, "..", "uploads", imageName));
-  // res.set("Content-Type", "image/jpeg");
-  // res.sendFile(path.join(__dirname, "..", "uploads", imageName));
 });
 
 router.delete("/:firmId", firmController.deleteFirmById);
